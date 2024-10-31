91792dad284719ab6505a260ae6d8c8527e41151
calebrhuhm <calebrh@hawaii.edu>
2024-10-31 13:13:38 -1000

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,40 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { PeopleFill, FileEarmarkTextFill, Calendar2CheckFill } from 'react-bootstrap-icons';
 
-/** The Home page. */
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
-      </Row>
+      <Container>
+        <Row className="align-items-center text-center">
+          {/* First Column */}
+          <Col xs={4}>
+            <PeopleFill size={100} />
+            <h1>Multiple Users</h1>
+            <h5>
+              This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.
+            </h5>
+          </Col>
+          
+          {/* Second Column */}
+          <Col xs={4}>
+            <FileEarmarkTextFill size={100} />
+            <h1>Contact Information</h1>
+            <h5>
+              For each contact, you can save their name, address, and phone number.
+            </h5>
+          </Col>
+          
+          {/* Third Column */}
+          <Col xs={4}>
+            <Calendar2CheckFill size={100} />
+            <h1>Contact Notes</h1>
+            <h5>
+              Each time you make contact with a contact, you can write a note that summarizes the conversation. This note is saved along with a timestamp.
+            </h5>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   </main>
 );
